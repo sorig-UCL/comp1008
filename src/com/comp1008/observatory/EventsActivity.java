d809a3dd030522ea24c69b552a9960e8a1d07efc
Kenny Chen <sakill@me.com>
2013-04-16 20:56:29 +0100

    Events

--- src/com/comp1008/observatory/EventsActivity.java
+++ src/com/comp1008/observatory/EventsActivity.java
@@ -22,7 +22,6 @@ public class EventsActivity extends Activity {
 		}
 	}
 	
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
