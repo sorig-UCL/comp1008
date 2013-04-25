d9006ed345f1bfdb272b843074f26c316d70f035
Olivier van Donselaar <ovandonselaar@hotmail.com>
2013-04-25 23:47:44 +0100

    telescopes infrastructure

--- src/com/comp1008/observatory/TelescopesActivity.java
+++ src/com/comp1008/observatory/TelescopesActivity.java
@@ -3,8 +3,11 @@ package com.comp1008.observatory;
 import android.os.Bundle;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.support.v4.app.NavUtils;
 import android.os.Build;
 
@@ -15,6 +18,7 @@ public class TelescopesActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_telescopes);
+		this.initialiseButtons();
 		// Show the Up button in the action bar.
 		
 		if (Build.VERSION.SDK_INT >= 11) {
@@ -25,7 +29,7 @@ public class TelescopesActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_research, menu);
+		getMenuInflater().inflate(R.menu.activity_telescopes, menu);
 		return true;
 	}
 
@@ -45,5 +49,42 @@ public class TelescopesActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	private void initialiseButtons() {
+		Button telescope1Button = (Button) findViewById(R.id.telescope1Button);
+		telescope1Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent intent = new Intent(TelescopesActivity.this, Telescope1Activity.class);
+       	     	startActivity(intent);
+            }
+        });
+		
+		Button telescope2Button = (Button) findViewById(R.id.telescope2Button);
+		telescope2Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent intent = new Intent(TelescopesActivity.this, Telescope2Activity.class);
+       	     	startActivity(intent);
+            }
+        });
+		
+		Button telescope34Button = (Button) findViewById(R.id.telescope34Button);
+		telescope34Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent intent = new Intent(TelescopesActivity.this, Telescope34Activity.class);
+       	     	startActivity(intent);
+            }
+        });
 
+		Button telescope5Button = (Button) findViewById(R.id.telescope5Button);
+		telescope5Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            	Intent intent = new Intent(TelescopesActivity.this, Telescope5Activity.class);
+       	     	startActivity(intent);
+            }
+        });
+	}
 }
+
